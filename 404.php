471d4860b1169a9076de18c564c682548c11cbbe
Shima Shinatama <shinatamasama@protonmail.com>
2020-12-26 16:07:06 -0500

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>404</title>
+    <link rel="stylesheet" href="/styles/fonts.css" />
+    <link rel="stylesheet" href="/styles/cursor.css" />
+    <link rel="stylesheet" href="/styles/404.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+    <marquee scrollamount="10">
+        <h1>uh oh</h1>
+    </marquee>
+    <div style="text-align: center; margin-left: auto; margin-right: auto;">
+    <span class="notfound">404 - not found..</span>
+    <p>There is nothing here.</p>
+    <p>I think we are lost.</p>
+    <div class="lostimg">
+        <a class="lostimglink" href="/home.php">
+            Take me home<br />
+            country road
+        </a>
+        <a href="/home.php"><img src="/images/404.gif" /></a>
+    </div>
+    </div>
+</body>
